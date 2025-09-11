a92cadce498e3d81ea85b90be65a5d266eab1a72
Sebastiaan <sebastiaan.hagoort@hva.nl>
2025-09-11 11:59:18 +0200

    #1
    place items naar midden

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -7,11 +7,11 @@
 <!-- <p>Hallo Hallo</p> -->
 <ProfileCard />
 
-<main style="background-color:{member.fav_color};">
-    <article>
+<main>
+    <article style="background-color:{member.fav_color};">
         <img src={member.avatar} alt="" width="200" height="200" />
         <h1>{member.name}</h1>
-        <p></p>
+        <p>{member.bio}</p>
     </article>
 </main>
 
@@ -19,10 +19,31 @@
     * {
         color: white;
     }
+
     main {
+        display: grid;
+        place-items: center;
+        @media (width < 32em) {
+            article {
+                max-width: 24em;
+            }
+            img {
+                height: 150px;
+                width: 150px;
+            }
+        }
     }
     article {
+        max-width: 32em;
         display: grid;
         grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 1fr;
+        img {
+            grid-row: 1/3;
+            grid-column: 1/2;
+        }
+        p {
+            grid-row: 2/2;
+        }
     }
 </style>
