2444702327c05f4d7ddfe478f53f4d89bfd8e9f7
Sebastiaan <sebastiaan.hagoort@hva.nl>
2025-09-11 11:42:57 +0200

    #1 wat vet
    wowzers

--- my-app/src/lib/components/ProfileCard.svelte
+++ my-app/src/lib/components/ProfileCard.svelte
@@ -1,1 +1,1 @@
-<h1>Naam + Naam</h1>
+<!-- <h1>Naam + Naam</h1> -->
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,9 +4,25 @@
     import ProfileCard from "$lib/components/ProfileCard.svelte";
 </script>
 
-<p>Hallo Hallo</p>
+<!-- <p>Hallo Hallo</p> -->
+<ProfileCard />
 
-<main>
-    <h1>{member.name}</h1>
-    <ProfileCard />
+<main style="background-color:{member.fav_color};">
+    <article>
+        <img src={member.avatar} alt="" width="200" height="200" />
+        <h1>{member.name}</h1>
+        <p></p>
+    </article>
 </main>
+
+<style>
+    * {
+        color: white;
+    }
+    main {
+    }
+    article {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+</style>
